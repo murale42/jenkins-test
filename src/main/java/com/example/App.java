5bb4ea63cebe9bfa9a7d9c4139165044626c3cc2
Aleksandra <alexandra42kuz@gmail.com>
2025-10-23 19:16:20 +0700

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,17 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class App extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello from Jetty + Jenkins + Maven!</h2>");
+        out.println("</body></html>");
     }
 }
